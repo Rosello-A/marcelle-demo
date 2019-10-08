5e2d58a8da9b1630f44e1e102dd9f32392853770
regis <regis.s.fontaine@gmail.com>
2019-10-08 16:26:05 +0200

    base landingpage

---
+++ pages/landing_page.vue
@@ -0,0 +1,24 @@
+<!-- pages/le_nom_de_ma_page.vue => localhost:3000/le_nom_de_ma_page o-->
+<template>
+  <div id="landing-page">
+    <section class="categories">NAV</section>
+    <section class="categories">JUMBOTRON</section>
+    <section class="categories">VISUEL+FONCTIONS</section>
+    <section class="categories">ECO FRIENDLY</section>
+    <section class="categories">USER REVIEW</section>
+    <section class="categories">GET STARTED</section>
+    <section class="categories">FOOTER</section>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'Prout'
+    }
+  }
+}
+</script>
+<style scoped>
+/* ur style here */
+</style>
